2db02a7c19098727cea1544e90077c98204796f7
quincy <insta0430@gmail.com>
2022-05-21 15:14:55 +0900

    .

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("submit").addEventListener('click', runApp);
+
+function runApp(){
+    document.getElementById("notice").style.display = "flex"
+    document.getElementById("notice").style.opacity = "100%"
+}
