e0fe67a44f7579ac0c20a8e92b54f382a5910011
quincy <insta0430@gmail.com>
2022-05-21 16:25:09 +0900

    .

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-document.getElementById("submit").addEventListener('click', runApp);
-
 function runApp(){
     document.getElementById("notice").style.zIndex = "1"
     document.getElementById("notice").style.opacity = "100%"
